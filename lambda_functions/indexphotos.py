b27e3ded662d8d196f4cd11d1bef5b7781dcf38a
Kiyoon Jeong <kiyoon.jeong@nyu.edu>
2020-12-16 16:11:58 -0500

    Update indexphotos.py

--- lambda_functions/indexphotos.py
+++ lambda_functions/indexphotos.py
@@ -9,6 +9,7 @@ def lambda_handler(event, context):
     print(event)
     print("hello22")
     print("checkifitupdates")
+    print("doublecheck")
     client = boto3.client('rekognition')
     s3_info = event['Records'][0]['s3']
     bucket = s3_info['bucket']['name']
